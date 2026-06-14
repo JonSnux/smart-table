9cc016f5b51069058c3d0c3dfc0dbfd8c65da587
Artem Morozov <teed3074@yandex.ru>
2026-06-14 13:15:48 +0300

    version one

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,16 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import {createComparison, rules} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
-    return (data, state, action) => {
+    const compare = createComparison(
+        ['skipEmptyTargetValues'],
+        [rules.searchMultipleFields(
+            searchField, 
+            ['date', 'customer', 'seller'], 
+            false)]
+    );
+    return (data, state) => {
         // @todo: #5.2 — применить компаратор
-        return data;
-    }
+        return data.filter((row) => compare(row, state));
+    };
 }
